6c22fcfe1e9dc76cf625e4f5ca4ac2a3bdc3ff6d
ruosen <18687269789@163.com>
2019-09-28 13:49:54 +0800

    springboot 集成swagger2

---
+++ src/main/java/com/ruosen/star/ruosenstar/RuosenStarApplication.java
@@ -0,0 +1,16 @@
+package com.ruosen.star.ruosenstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Administrator
+ */
+@SpringBootApplication
+public class RuosenStarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuosenStarApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/ruosen/star/ruosenstar/config/Swagger2Config.java
@@ -0,0 +1,48 @@
+package com.ruosen.star.ruosenstar.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ *  设置 dev test 环境开启 prod 环境就关闭了
+ *  * @projectName ruosen-star
+ *  * @title     Swagger2Config   
+ *  * @package    com.ruosen.star.ruosenstar.config  
+ *  * @author Administrator     
+ *  * @date   2019/9/28 0028 星期六
+ *  * @version V1.0.0
+ *  
+ */
+@Configuration
+@EnableSwagger2
+@Profile({"dev", "test"})
+public class Swagger2Config {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.ruosen.star.ruosenstar.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("springboot利用swagger构建api文档")
+                .description("简单优雅的restfun风格")
+                // 作者信息
+                .contact(new Contact("罗先波", "", "809704354@qq.com"))
+                .version("1.0")
+                .build();
+    }
+}
